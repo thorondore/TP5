95fd8b228b3ad3a68baa90b852e4d571c1521dd0
Elisabeth <lili.berat@orange.fr>
2023-11-24 10:33:18 +0100

    clean code

--- tp5.c
+++ tp5.c
@@ -78,7 +78,7 @@ int pm_getint(FILE* file) {
 }
 
 /*---------------------------------------------------------------------------*/
-/*                              functioooooooons                             */
+/*                              TP5                                          */
 /*---------------------------------------------------------------------------*/
 
 struct point3d rotation(struct point3d coordinate, float gama, float beta, float sigma, float T_x, float T_y, float T_z){
@@ -114,34 +114,125 @@ struct point2d perspective_projection(float f, struct point3d coordinate, float 
     return new_point;
 }
 
+struct point2d orthogonal_projection(struct point3d coordinate, float alpha, float u0, float v0, float threshold, color * final_map){
+
+    struct point2d new_point;
+    new_point.x = ((coordinate.x)/alpha) + u0;
+    new_point.y = ((coordinate.y)/alpha) + v0;
+    int coord = (((int) new_point.y * x_size) + (int)(new_point.x)) * 3;
+
+    /*We select the point that are the closer to us*/
+
+    if (coordinate.z > threshold){
+        new_point.red = final_map[coord+3];
+        new_point.green = final_map[coord+4];
+        new_point.blue = final_map[coord+5];
+    }
+
+    else{
+        new_point.red = coordinate.r;
+        new_point.green = coordinate.g;
+        new_point.blue = coordinate.b;
+        }
+
+    return new_point;
+}
+
+color *fill_holes(int index, color *final_map, struct point2d point){
+    
+    /* If the points surrounding the pixel that is not bmaclk are blacks, we replace their color*/
+
+    if(final_map[index+3]==0 && final_map[index+4]==0 && final_map[index+5]==0){
+        final_map[index+3] = point.red;
+        final_map[index + 4] = point.green;
+        final_map[index + 5] = point.blue;
+    }
+
+    if(final_map[index-3]==0 && final_map[index-2]==0 && final_map[index-1]==0){
+        final_map[index-3] = point.red;
+        final_map[index -2] = point.green;
+        final_map[index -1] = point.blue;
+    }
+
+    if(final_map[index+x_size*3]==0 && final_map[index+x_size*3+1]==0 && final_map[index+x_size*3+2]==0){
+        final_map[index+x_size*3] = point.red;
+        final_map[index + x_size*3+1] = point.green;
+        final_map[index + x_size*3+2] = point.blue;
+    }
+
+    if(final_map[index-x_size*3]==0 && final_map[index-x_size*3+1]==0 && final_map[index-x_size*3+2]==0){
+        final_map[index-x_size*3] = point.red;
+        final_map[index -x_size*3+1] = point.green;
+        final_map[index -   x_size*3+2] = point.blue;
+    }
+        
+
+    return final_map;
+}
+
+
 
 int main(int argc, char* argv[]) {
     float f;
 
     float u0 = x_size/2.0;
     float v0 = y_size/2.0;
-    float alpha = 1.0/x_size;
+    float alpha = 4.0/x_size;
     struct point3d rotated_point;
+    float threshold;
+    int projection;
 
-    printf("Select the desired focal value :\n");
-    scanf("%f", &f);
 
+    printf("Select the desired projection value :\n 1 --> perspectve\n 2 --> orthogonal\n");
+    scanf("%i", &projection);
+
+    if(projection!=1 && projection!=2){
+        printf("Unvalid value");
+        exit(1);
+    }
+
+    if (projection==2){
+        printf("Select the desired threshold value :\n");
+        scanf("%f", &threshold);
+    }
+
+    if (projection==1){
+        printf("Select the desired focal value :\n");
+        scanf("%f", &f);
+    }
+    
     struct point3d *points;
     int N_v = 0;
     points = readOff(argv[1], &N_v);
 
-    color* final_map = calloc(1920 * 1080 * 3, sizeof(color));
+    color* final_map = calloc(x_size * y_size * 3, sizeof(color));
     struct point2d point;
     for (int i = 0; i < N_v; i++) {
         rotated_point = rotation(points[i], 0, 0, 0, 0, 0, 0);
-        point = perspective_projection(f, rotated_point, alpha, u0, v0);
+        
+        if (projection==1){
+            point = perspective_projection(f, rotated_point, alpha, u0, v0);
+        }
+
+        else{
+            point = orthogonal_projection(rotated_point, alpha, u0, v0, threshold, final_map);
+        }
+        
         if (point.x < 0 || point.x >= x_size || point.y < 0 || point.y >= y_size) {
             continue;
         }
-        int coord = (((int) point.y * 1920) + (int)(point.x)) * 3;
+
+        int coord = (((int) point.y * x_size) + (int)(point.x)) * 3;
+        
         final_map[coord] = point.red;
         final_map[coord + 1] = point.green;
         final_map[coord + 2] = point.blue;
+
+        /* In the case of an orthogonal projection, we fill the holes created by the projection*/
+        if (projection==2 && (final_map[coord]!=0 || final_map[coord+1]!=0 || final_map[coord+2]!=0)){
+            fill_holes(coord, final_map, point);
+        }
+        
     }
 
     /* Opening output file */
@@ -153,20 +244,20 @@ int main(int argc, char* argv[]) {
 
     /* Write file header */
     fprintf(file_output, "%s\n", "P3");
-    fprintf(file_output, "%d %d\n", 1920, 1080);
+    fprintf(file_output, "%d %d\n", x_size, y_size);
     fprintf(file_output, "%d\n", 255);
 
 
     /* We write to the output file */
-    for (long int i = 0; i < 1920 * 1080 * 3; i++) {
+    for (long int i = 0; i < x_size * y_size * 3; i++) {
         fprintf(file_output, "%d ", final_map[i]);
     }
 
     fclose(file_output);
 
-    // centerThePCL(points, N_v);
-
     return EXIT_SUCCESS;
 } 
+
+
 
 
---
+++ tp5_Paoli_Berat.md
@@ -0,0 +1,16 @@
+# TP5: Image Formation
+## Exercise 1: Implementation
+
+### Question 1.  How does the image change if we modify the following parameters: focal length, object transform, image center, and image resolution ?
+
+
+Here are the result we got for onmy changing the focal. The focal values are (from left to right and up to down): 0.1, 0.3, 0.5, 1, 2, 10.
+
+<img src="Focal_0.1.png" width="300">
+<img src="Focal_0.3.png" width="300">
+<img src="Focal_0.5.png" width="300">
+<img src="Focal_1.png" width="300">
+<img src="Focal_2.png" width="300">
+<img src="Focal_10.png" width="300">
+
+We can see that increasing the focal makes the object of the image bigger. it also reduces the image depth. It makes sens when we look at the formula : the smaller the focal is, the bigger the denominator is. Furthermore, when f tends toward infinity, X_img and Y_img have X_p' and Y_p' as limits.
